1a3eb2dfa971f8a5e009b2b11c3d34ea5d5e76d4
alex <Dubik90@yandex.by>
2022-12-13 17:24:39 +0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            JORA BORA
+            JORA BORA !!!!!!!!!!!!!!!!!!!!!!!!!!!
         </div>
     );
 }
