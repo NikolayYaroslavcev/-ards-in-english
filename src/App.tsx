49b5d232d4cde96c67ecc7d981a4d05e8a50852c
Nikolay <sweeterthanfurby@mail. git config --global user.email sweeterthanfurby@mail>
2022-12-15 11:36:32 +0300

    refactor:home

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import {Registration} from './features/auth/Registration';
 import {Logging} from './features/auth/Logging';
 import {Route, Routes} from 'react-router-dom';
 import {Profile} from "./features/profile/Profile";
-import {authAPI} from "./features/auth/auth-api";
+import {Home} from './features/auth/Home';
+
+
 
 function App() {
+
+
 
     useEffect(() => {
 
     }, [])
+
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<div>dffdgfdgdfgdfgdfgewrewreewrwre</div>}/>
+                <Route path="/" element={<Home/>}/>
                 {/*<Navigation/>*/}
                 <Route path="/login" element={<Logging/>}/>
                 <Route path="/registration" element={<Registration/>}/>
