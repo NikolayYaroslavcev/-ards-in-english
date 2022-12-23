64fcba9c7b8721b4a597699896293c377a46ccb5
alex <Dubik90@yandex.by>
2022-12-23 14:16:50 +0300

    refactor: styled components fix

--- src/features/deck/Desk.tsx
+++ src/features/deck/Desk.tsx
@@ -2,11 +2,12 @@ import React, {useEffect} from 'react';
 import {deckTC} from './desk-reducer';
 import {logOutTC} from '../auth/auth-reducer';
 import {useAppDispatch, useAppSelector} from '../../common/hooks/hooks';
+import {DeckType} from "./desk-api";
 
 export const Desk = () => {
 
 
-    const decks = useAppSelector(state => state.deck.deck)
+    const decks = useAppSelector(state => state.deck.cardPacks)
     console.log(decks)
     const dispatch = useAppDispatch()
 
@@ -17,10 +18,12 @@ export const Desk = () => {
 
 
     return (
-        <div>
-            {/*{decks?.cardPacks.map((el))}*/}
-
-
+        <div> 
+            {decks.map((el => {
+                return (
+                    <div key={el._id}>{el.name}</div>
+                )
+            }))}
         </div>
     );
 };
--- src/features/deck/desk-reducer.ts
+++ src/features/deck/desk-reducer.ts
@@ -1,19 +1,25 @@
 import {createSlice, Dispatch, PayloadAction} from '@reduxjs/toolkit';
 import {AxiosError} from 'axios';
-import {deskApi} from './desk-api';
+import {DeckType, deskApi} from './desk-api';
 
 
-const initialState = {
-    deck: {}
+const initialState: InitialStateType = {
+    cardPacks: [],
+    minCardsCount:0,
+    pageCount: 0,
+    cardPacksTotalCount: 0,
+    page: 0,
+    maxCardsCount: 0
 }
 
-
+type InitialStateType = DeckType
 const slice = createSlice({
     name: 'auth',
     initialState: initialState,
     reducers: {
         getDeckAC(state, action: PayloadAction<{}>) {
-            state.deck = action.payload
+            return {...state, ...action.payload}
+            //state = {...action.payload}
         }
     }
 })
@@ -23,7 +29,7 @@ export const deckTC = () => async (dispatch: Dispatch) => {
     try {
         const res = await deskApi.desk()
         dispatch(getDeckAC(res.data))
-        console.log(res.data)
+        //console.log(res.data)
     } catch (e) {
         const err = e as Error | AxiosError
         console.log(err)
